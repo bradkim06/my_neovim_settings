141ba287951821fb82c262c9094d825fa9f6c367
bradkim06 <bradkim06@gmail.com>
2023-11-15 11:44:19 +0900

    Add conform.nvim plugin and disable coc.preferences.formatOnSave and coc.preferences.formatOnType for markdown, lua, and c. Adjust indentation in plugin.lua.

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,71 +1,73 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- main colorscheme
 local colorscheme = {
-    "rebelot/kanagawa.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        -- load the colorscheme here
-        vim.cmd("colorscheme kanagawa-wave")
-    end,
+	"rebelot/kanagawa.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		-- load the colorscheme here
+		vim.cmd("colorscheme kanagawa-wave")
+	end,
 }
 
 require("lazy").setup({
-    -- colorscheme list
-    "catppuccin/nvim",
-    colorscheme,
-    -- showing changes in the sign column.
-    require("plugins.gitsigns_nvim"),
-    -- for realtime markdown previewing
-    "iamcco/markdown-preview.nvim",
-    -- provides functionalities to easily delete, change and add surroundings in pairs.
-    "tpope/vim-surround",
-    -- provides sensible default settings for Vim.
-    "tpope/vim-sensible",
-    -- Neofor easily reviewing and navigating diffs.
-    "sindrets/diffview.nvim",
-    -- used to enhance the search functionality in Neovim.
-    require("plugins.wilder_nvim"),
-    -- generating Doxygen documentation quickly and easily.
-    require("plugins.doxygentoolkit_vim"),
-    -- integrating the lazygit terminal UI within the Neovim environment.
-    require("plugins.lazygit_nvim"),
-    -- tab-like buffer line with close icons and buffer sorting.
-    require("plugins.bufferline_nvim"),
-    -- powerful, extensible Vim/Neofor autocompletion, linting, and language server protocol support.
-    require("plugins.coc_nvim"),
-    -- syntax highlighting, indentation, and code navigation in Neovim using the Tree-sitter parser.
-    require("plugins.nvim_treesitter"),
-    -- adding, deleting, and navigating through comments in Neovim.
-    require("plugins.comment_nvim"),
-    -- A Neovim status line plugin written in Lua for better performance and customization.
-    require("plugins.lualine_nvim"),
-    -- allows you to search and replace text across multiple files.
-    require("plugins.nvim_spectre"),
-    -- integrate AI features into the Vim text editor.
-    require("plugins.vim_ai"),
-    -- functionality for colorizing text in Neovim.
-    require("plugins.nvim_colorizer_lua"),
-    -- reopen files at the last edited position.
-    require("plugins.nvim_lastplace"),
-    -- fuzzy finder over lists for Neovim.
-    require("plugins.telescope_nvim"),
-    -- toggle the terminal window on and off.
-    require("plugins.toggle_term"),
-    -- execution of code snippets directly from the text editor.
-    require("plugins.sniprun"),
-    -- provides a pop-up menu for keybindings to enhance workflow efficiency in Vim.
-    require("plugins.which_key_nvim"),
+	-- colorscheme list
+	"catppuccin/nvim",
+	colorscheme,
+	-- automatically format your code according to a specified style guide.
+	require("plugins.conform_nvim"),
+	-- showing changes in the sign column.
+	require("plugins.gitsigns_nvim"),
+	-- for realtime markdown previewing
+	"iamcco/markdown-preview.nvim",
+	-- provides functionalities to easily delete, change and add surroundings in pairs.
+	"tpope/vim-surround",
+	-- provides sensible default settings for Vim.
+	"tpope/vim-sensible",
+	-- Neofor easily reviewing and navigating diffs.
+	"sindrets/diffview.nvim",
+	-- used to enhance the search functionality in Neovim.
+	require("plugins.wilder_nvim"),
+	-- generating Doxygen documentation quickly and easily.
+	require("plugins.doxygentoolkit_vim"),
+	-- integrating the lazygit terminal UI within the Neovim environment.
+	require("plugins.lazygit_nvim"),
+	-- tab-like buffer line with close icons and buffer sorting.
+	require("plugins.bufferline_nvim"),
+	-- powerful, extensible Vim/Neofor autocompletion, linting, and language server protocol support.
+	require("plugins.coc_nvim"),
+	-- syntax highlighting, indentation, and code navigation in Neovim using the Tree-sitter parser.
+	require("plugins.nvim_treesitter"),
+	-- adding, deleting, and navigating through comments in Neovim.
+	require("plugins.comment_nvim"),
+	-- A Neovim status line plugin written in Lua for better performance and customization.
+	require("plugins.lualine_nvim"),
+	-- allows you to search and replace text across multiple files.
+	require("plugins.nvim_spectre"),
+	-- integrate AI features into the Vim text editor.
+	require("plugins.vim_ai"),
+	-- functionality for colorizing text in Neovim.
+	require("plugins.nvim_colorizer_lua"),
+	-- reopen files at the last edited position.
+	require("plugins.nvim_lastplace"),
+	-- fuzzy finder over lists for Neovim.
+	require("plugins.telescope_nvim"),
+	-- toggle the terminal window on and off.
+	require("plugins.toggle_term"),
+	-- execution of code snippets directly from the text editor.
+	require("plugins.sniprun"),
+	-- provides a pop-up menu for keybindings to enhance workflow efficiency in Vim.
+	require("plugins.which_key_nvim"),
 })
---
+++ lua/plugins/conform_nvim.lua
@@ -0,0 +1,56 @@
+local conform_nvim = {
+	"stevearc/conform.nvim",
+	opts = {},
+}
+
+conform_nvim.config = function()
+	require("conform").setup({
+		-- Map of filetype to formatters
+		formatters_by_ft = {
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+			dts = { "codespell" },
+			cmake = { "cmake_format" },
+			sh = { "shfmt" },
+			lua = { "stylua" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { { "prettierd", "prettier" } },
+			-- You can use a function here to determine the formatters dynamically
+			python = function(bufnr)
+				if require("conform").get_formatter_info("ruff_format", bufnr).available then
+					return { "ruff_format" }
+				else
+					return { "isort", "black" }
+				end
+			end,
+			markdown = { "mdformat" },
+			json = { "prettierd" },
+			jsonc = { "prettierd" },
+			-- Use the "*" filetype to run formatters on all filetypes.
+			["*"] = { "codespell" },
+			-- Use the "_" filetype to run formatters on filetypes that don't
+			-- have other formatters configured.
+			["_"] = { "trim_whitespace" },
+		},
+		-- If this is set, Conform will run the formatter on save.
+		-- It will pass the table to conform.format().
+		-- This can also be a function that returns the table.
+		format_on_save = {
+			-- I recommend these options. See :help conform.format for details.
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+		-- If this is set, Conform will run the formatter asynchronously after save.
+		-- It will pass the table to conform.format().
+		-- This can also be a function that returns the table.
+		format_after_save = {
+			lsp_fallback = true,
+		},
+		-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+		log_level = vim.log.levels.ERROR,
+		-- Conform will notify you when a formatter errors
+		notify_on_error = true,
+	})
+end
+
+return conform_nvim
