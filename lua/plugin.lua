50e89f91433f5241294eecdb9ea2bbdde176a979
bradkim06 <bradkim06@gmail.com>
2023-11-15 10:26:17 +0900

    Refactor: Simplify plugin descriptions in lua/plugin.lua
    This commit simplifies the descriptions of various plugins in the lua/plugin.lua file. The changes remove redundant phrases such as "This Vim plugin" and "is a Vim plugin" from the descriptions, making them more concise and easier to read.

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -26,44 +26,44 @@ require("lazy").setup({
     -- colorscheme list
     "catppuccin/nvim",
     colorscheme,
-    -- "iamcco/markdown-preview.nvim" is a Vim plugin for realtime markdown previewing
+    -- for realtime markdown previewing
     "iamcco/markdown-preview.nvim",
-    -- "tpope/vim-surround" is a Vim plugin that provides functionalities to easily delete, change and add surroundings in pairs.
+    -- provides functionalities to easily delete, change and add surroundings in pairs.
     "tpope/vim-surround",
-    -- "tpope/vim-sensible" is a Vim plugin that provides sensible default settings for Vim.
+    -- provides sensible default settings for Vim.
     "tpope/vim-sensible",
-    -- "sindrets/diffview.nvim" is a Neovim plugin for easily reviewing and navigating diffs.
+    -- Neofor easily reviewing and navigating diffs.
     "sindrets/diffview.nvim",
-    -- This Vim plugin is used to enhance the search functionality in Neovim.
+    -- used to enhance the search functionality in Neovim.
     require("plugins.wilder_nvim"),
-    -- "vim-scripts/DoxygenToolkit.vim": a Vim plugin for generating Doxygen documentation quickly and easily.
+    -- generating Doxygen documentation quickly and easily.
     require("plugins.doxygentoolkit_vim"),
-    -- "kdheepak/lazygit.nvim" is a Vim plugin for integrating the lazygit terminal UI within the Neovim environment.
+    -- integrating the lazygit terminal UI within the Neovim environment.
     require("plugins.lazygit_nvim"),
-    -- 'akinsho/bufferline.nvim' is a Vim plugin that provides a tab-like buffer line with close icons and buffer sorting.
+    -- tab-like buffer line with close icons and buffer sorting.
     require("plugins.bufferline_nvim"),
-    -- "neoclide/coc.nvim" is a powerful, extensible Vim/Neovim plugin for autocompletion, linting, and language server protocol support.
+    -- powerful, extensible Vim/Neofor autocompletion, linting, and language server protocol support.
     require("plugins.coc_nvim"),
-    -- This plugin 'nvim-treesitter/nvim-treesitter' is used for syntax highlighting, indentation, and code navigation in Neovim using the Tree-sitter parser.
+    -- syntax highlighting, indentation, and code navigation in Neovim using the Tree-sitter parser.
     require("plugins.nvim_treesitter"),
-    -- This Vim plugin, 'numToStr/Comment.nvim', is used for adding, deleting, and navigating through comments in Neovim.
+    -- adding, deleting, and navigating through comments in Neovim.
     require("plugins.comment_nvim"),
     -- A Neovim status line plugin written in Lua for better performance and customization.
     require("plugins.lualine_nvim"),
-    -- This Vim plugin, "nvim-spectre", is a tool for Neovim that allows you to search and replace text across multiple files.
+    -- allows you to search and replace text across multiple files.
     require("plugins.nvim_spectre"),
-    -- This Vim plugin, "madox2/vim-ai", is used to integrate AI features into the Vim text editor.
+    -- integrate AI features into the Vim text editor.
     require("plugins.vim_ai"),
-    -- This Vim plugin ("norcalli/nvim-colorizer.lua") provides functionality for colorizing text in Neovim.
+    -- functionality for colorizing text in Neovim.
     require("plugins.nvim_colorizer_lua"),
-    -- This Vim plugin "ethanholz/nvim-lastplace" helps to reopen files at the last edited position.
+    -- reopen files at the last edited position.
     require("plugins.nvim_lastplace"),
-    -- This plugin 'nvim-telescope/telescope.nvim' is a highly extensible fuzzy finder over lists for Neovim.
+    -- fuzzy finder over lists for Neovim.
     require("plugins.telescope_nvim"),
-    -- This Vim plugin allows you to toggle the terminal window on and off.
+    -- toggle the terminal window on and off.
     require("plugins.toggle_term"),
-    -- This Vim plugin, Sniprun, allows for the execution of code snippets directly from the text editor.
+    -- execution of code snippets directly from the text editor.
     require("plugins.sniprun"),
-    -- "folke/which-key.nvim" is a Vim plugin that provides a pop-up menu for keybindings to enhance workflow efficiency in Vim.
+    -- provides a pop-up menu for keybindings to enhance workflow efficiency in Vim.
     require("plugins.which_key_nvim"),
 })
