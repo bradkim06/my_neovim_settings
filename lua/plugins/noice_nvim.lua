7a816a4dfc5f9be89f20e49c7478d71c99d78ac9
bradkim06 <bradkim06@gmail.com>
2023-11-16 11:17:12 +0900

    Commit: Update noice_nvim plugin configuration
    This commit modifies the configuration of the noice_nvim plugin. It adds a new format for the cmdline view, including patterns and icons for different command types such as search, filter, lua, and help. It also changes the settings for the command_palette and lsp_doc_border presets.

--- lua/plugins/noice_nvim.lua
+++ lua/plugins/noice_nvim.lua
@@ -26,14 +26,23 @@ noice_nvim.config = function()
 		},
 		cmdline = {
 			view = "cmdline",
+
+			format = {
+				cmdline = { pattern = "^:", icon = " cmd", lang = "vim" },
+				search_down = { kind = "search", pattern = "^/", icon = "search 🔽", lang = "regex" },
+				search_up = { kind = "search", pattern = "^%?", icon = "search 🔼", lang = "regex" },
+				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = " lua", lang = "lua" },
+				help = { pattern = "^:%s*he?l?p?%s+", icon = "❓ help" },
+			},
 		},
 		-- you can enable a preset for easier configuration
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
+			command_palette = false, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 	})
 end
