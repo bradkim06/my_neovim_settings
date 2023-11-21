60721602f426cd286107c612703b7b5c57b46628
bradkim06 <bradkim06@gmail.com>
2023-11-21 18:08:54 +0900

    Subject: Add project.nvim and its telescope extension to the plugin list
    In this commit, the project.nvim, a neovim plugin developed by ahmedkhalf, has been added to the lazy plugin setup. This plugin is dependent on the nvim-telescope/telescope.nvim. The telescope extension named "projects" has also been loaded as part of the setup. The default settings of the project_nvim have been used in the configuration. Any specific configuration can be added in the provided space.

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -22,6 +22,18 @@ require("lazy").setup({
 		config = true,
 		event = "VeryLazy",
 	},
+	{
+		"ahmedkhalf/project.nvim",
+		dependencies = "nvim-telescope/telescope.nvim",
+		config = function()
+			require("project_nvim").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+			require("telescope").load_extension("projects")
+		end,
+	},
 	-- ===============================================================================================
 	-- Colorscheme List
 	-- ===============================================================================================
