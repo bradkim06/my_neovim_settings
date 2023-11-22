8454de2209e7184554b7d05626ea61f29395a75d
bradkim06 <bradkim06@gmail.com>
2023-11-22 14:06:45 +0900

    Subject: Simplify project_nvim setup in plugin.lua

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -26,11 +26,7 @@ require("lazy").setup({
 		"ahmedkhalf/project.nvim",
 		dependencies = "nvim-telescope/telescope.nvim",
 		config = function()
-			require("project_nvim").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
+			require("project_nvim").setup({})
 			require("telescope").load_extension("projects")
 		end,
 	},
