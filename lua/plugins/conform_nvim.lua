e85aa6029aed9540ecd4e75b123eda332148739e
bradkim06 <bradkim06@gmail.com>
2025-04-28 14:48:22 +0900

    conform bug fix(codespell remove)

--- lua/plugins/conform_nvim.lua
+++ lua/plugins/conform_nvim.lua
@@ -18,44 +18,23 @@ conform_nvim.config = function()
 			-- C 및 C++ 파일에 clang_format 사용
 			c = { "clang_format" },
 			cpp = { "clang_format" },
-
-			-- DTS 파일에 codespell 사용
-			dts = { "codespell" },
-
 			-- CMake 파일에 cmake_format 사용
 			cmake = { "cmake_format" },
-
 			-- 쉘 스크립트 파일에 shfmt 사용 (sh filetype)
 			sh = { "shfmt" },
-
 			-- Lua 파일에 stylua 사용
 			lua = { "stylua" },
-
 			-- Python 파일: ruff_format이 사용 가능한 경우 사용, 아니면 isort와 black 조합 사용
-			python = function(bufnr)
-				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "ruff_format" }
-				else
-					return { "black" }
-				end
-			end,
-
+			python = { "ruff" },
 			-- Markdown 파일에 mdformat 사용
 			markdown = { "mdformat" },
-
 			-- JSON 및 JSONC 파일에 prettierd 사용
 			json = { "prettierd" },
 			jsonc = { "prettierd" },
-
-			-- "*" 파일 타입: 모든 파일에 대해 codespell 실행
-			["*"] = { "codespell" },
-
 			-- "_" 파일 타입: 별도의 포매터가 없는 파일에 대해 trim_whitespace 실행
 			["_"] = { "trim_whitespace" },
-
 			-- 추가: bash 파일에 shfmt 사용
 			bash = { "shfmt" },
-
 			-- 추가: zsh 파일에 shfmt 사용
 			zsh = { "shfmt" },
 		},
